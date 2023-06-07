91513c01fbe62bc90f4d2fc51374c1e9cd2e1896
Aabill <domingoaaronbill@gmail.com>
2023-06-07 11:00:13 +0800

    Fix available services

--- includes/accomodation.php
+++ includes/accomodation.php
@@ -145,7 +145,8 @@ class Accomodation{
 
 	public function getAvailableServices($date) {
 		global $mydb;
-		$sql = "SELECT `ACCOMID`, `ACCOMODATION`, `tblreservation`.`ARRIVAL`, `tblreservation`.`STATUS`, DATE(`tblreservation`.`ARRIVAL`) as date FROM `tblaccomodation` LEFT JOIN `tblreservation` ON `tblreservation`.`ACCOMOID` = `tblaccomodation`.`ACCOMID` WHERE `tblreservation`.`STATUS` NOT IN ('Checkedin', 'Pending', 'Confirmed') OR `tblreservation`.`STATUS` IS NULL";
+		// $sql = "SELECT `ACCOMID`, `ACCOMODATION`, `tblreservation`.`ARRIVAL`, `tblreservation`.`STATUS`, DATE(`tblreservation`.`ARRIVAL`) as date FROM `tblaccomodation` LEFT JOIN `tblreservation` ON `tblreservation`.`ACCOMOID` = `tblaccomodation`.`ACCOMID` WHERE `tblreservation`.`STATUS` NOT IN ('Checkedin', 'Pending', 'Confirmed') OR `tblreservation`.`STATUS` IS NULL";
+		$sql = "SELECT `ACCOMID`, `ACCOMODATION`, `tblreservation`.`ARRIVAL`, `tblreservation`.`STATUS`, DATE(`tblreservation`.`ARRIVAL`) as date_arrival FROM `tblaccomodation` LEFT JOIN `tblreservation` ON `tblreservation`.`ACCOMOID` = `tblaccomodation`.`ACCOMID` WHERE (DATE(`tblreservation`.`ARRIVAL`) != '".$date."' OR `tblreservation`.`STATUS` IN ('Cancelled','Checkedout')) OR `tblreservation`.`STATUS` IS NULL";
 		$mydb->setQuery($sql);
 		$cur = $mydb->loadResultList();
 
